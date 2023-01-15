ad9fb4f0341f45189c603fcca739667b5751dc24
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-15 14:01:56 -0600

    import css styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+
 function App() {
   return (
     <div className="App">
-      <h1>Toolbox</h1>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+        <Container fluid>
+          <Navbar.Brand href="#home">Toolbox</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* 
+NOTE: If using SASS, will have to replace this with proper import.
+This is necessary for React Bootstrap styling
+*/
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
