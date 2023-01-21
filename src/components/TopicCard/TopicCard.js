6d46be3a2f7396cde77c1fca57b2e75842acd523
Tatia Burdett <tatia@Tatias-MacBook-Pro.local>
2023-01-21 08:57:54 -0600

    add test img

--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import './TopicCard.css';
+import img from '../../images/img.jpg'
+
 
 function TopicCard() {
 
     return (
         <div className='card-container'>
             <Card style={{ width: '18rem' }} className='card'>
-                <Card.Img variant='top' src='holder.js/100px180' />
+                <Card.Img variant='top' src={img} />
                 <Card.Body>
                     <Card.Title>Hello</Card.Title>
                     <Card.Text>Text here</Card.Text>
